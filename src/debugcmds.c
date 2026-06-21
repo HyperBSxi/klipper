d137fdcc270a5b9b099cf0245cae69ba9aa78440
HyperBSxi <h9085331@gmail.com>
2026-06-21 19:21:01 +0900

    debugcmds: Handle H5 info area reads safely
    Read the STM32H5 info area with aligned word accesses while\ntemporarily disabling ICACHE. This matches the existing H5\nchipid workaround and avoids disconnecting the MCU during\ndebug_read-based temperature_mcu calibration reads.\n\nSigned-off-by:@HyperBSxi <h9085331@gmail.com>

--- src/debugcmds.c
+++ src/debugcmds.c
@@ -4,17 +4,57 @@
 //
 // This file may be distributed under the terms of the GNU GPLv3 license.
 
+#include "autoconf.h" // CONFIG_MACH_STM32H5
 #include "board/io.h" // readl
 #include "board/irq.h" // irq_save
 #include "command.h" // DECL_COMMAND
+#if CONFIG_MACH_STM32H5
+#include "board/internal.h" // ICACHE
+
+#define H5_INFO_START 0x08FFF800
+#define H5_INFO_END   0x08FFF81f
+
+static uint32_t
+debug_read_stm32h5_info(uint8_t order, uint32_t addr)
+{
+    uint32_t aligned_addr = addr & ~0x3;
+    uint32_t cache_cr = ICACHE->CR;
+    if (cache_cr & ICACHE_CR_EN) {
+        ICACHE->CR = 0;
+        while (ICACHE->SR & ICACHE_SR_BUSYF)
+            ;
+    }
+    uint32_t word = *(volatile uint32_t*)aligned_addr;
+    if (cache_cr & ICACHE_CR_EN)
+        ICACHE->CR = ICACHE_CR_CACHEINV | ICACHE_CR_EN;
+    switch (order) {
+    default:
+    case 0:
+        return (word >> ((addr & 0x3) * 8)) & 0xff;
+    case 1:
+        return (word >> ((addr & 0x2) * 8)) & 0xffff;
+    case 2:
+        return word;
+    }
+}
+#endif
 
 void
 command_debug_read(uint32_t *args)
 {
     uint8_t order = args[0];
-    void *ptr = command_decode_ptr(args[1]);
+    uint32_t addr = args[1];
+    void *ptr = command_decode_ptr(addr);
     uint32_t v;
     irqstatus_t flag = irq_save();
+#if CONFIG_MACH_STM32H5
+    if (addr >= H5_INFO_START && addr <= H5_INFO_END) {
+        v = debug_read_stm32h5_info(order, addr);
+        irq_restore(flag);
+        sendf("debug_result val=%u", v);
+        return;
+    }
+#endif
     switch (order) {
     default: case 0: v = readb(ptr); break;
     case 1:          v = readw(ptr); break;
